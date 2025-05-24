8e669988652f84fe3c78cb03751e05fd89ea81db
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-05-24 08:12:42 +0530

    Initial user-service microservice

---
+++ src/main/java/org/mercurion/userservice/MercurionApplication.java
@@ -0,0 +1,16 @@
+package org.mercurion.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercurionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercurionApplication.class, args);
+    }
+
+
+    //postgresql://postgres:JfhuOuSNMWCMNYnDqzbwouWJSuGbKQli@shuttle.proxy.rlwy.net:10487/railway
+
+}
---
+++ src/test/java/org/mercurion/userservice/MercurionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mercurion.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercurionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
