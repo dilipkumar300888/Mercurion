75b5602a3986123146069c9d10dd20a3eea5095d
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-05-25 11:37:03 +0530

    Created User model

---
+++ src/main/java/org/mercurion/userservice/model/User.java
@@ -0,0 +1,22 @@
+package org.mercurion.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+    private String role;
+}
